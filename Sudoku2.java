589e75a107e64f554479631447ffc32372ff440c
JorgeLeon12 <jorgeleon12@hotmail.com>
2013-09-14 16:26:33 -0500

    Sudoku Tablero

---
+++ Sudoku2.java
@@ -0,0 +1,64 @@
+public class Sudoku2{
+	public static void main(String[] args){
+		System.out.println("    A   B   C     D   E   F     G   H   I");
+		System.out.print("  ");
+		System.out.println((char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187);
+		System.out.print("1 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("2 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("3 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188);
+		
+		System.out.print("  ");
+		System.out.println((char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187);
+		System.out.print("4 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("5 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("6 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188);
+		
+		System.out.print("  ");
+		System.out.println((char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187 + " " + (char)201 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)203 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)187);
+		System.out.print("7 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("8 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185 + " " + (char)204 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)206 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)185);
+		System.out.print("8 ");
+		System.out.println((char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186 + " 1 " + (char)186);
+		System.out.print("  ");
+		System.out.println((char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188 + " " + (char)200 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)202 + "" + (char)205 + "" + (char)205 + "" + (char)205 + "" + (char)188);
+	}
+}
+// + "" + (char)
+//    ++-++¦--¦¦+
+//http://www.asciitable.com/
+
+/*
++-----------+
+¦ x ¦   ¦   ¦
+¦---+---+---¦
+¦ x ¦   ¦   ¦
+¦---+---+---¦
+¦ x ¦   ¦   ¦
++-----------+
+
+++-++¦--¦¦+
+*/
