181af2a3b700fa815469385ed1d885a631288993
jeredesau <jeesrato@gmail.com>
2013-09-14 16:22:53 -0500

    sudoku tablero y casilla

---
+++ Casilla.java
@@ -0,0 +1,30 @@
+public class Casilla
+{
+   private int valor;
+   private boolean inicial;
+   private boolean utilizado;
+   
+
+   private static final int[] intvalor={0,1,2,3,4,5,6,7,8,9};    
+
+        public Casilla()
+          {
+           this.valor = 0;
+           this.inicial= false;
+           this.utilizado= false;
+          }
+              
+        public Casilla(int valor, boolean uno)
+          {
+          this.valor=valor;
+          this.inicial=uno;
+          this.utilizadp=true;
+
+          }
+       
+
+
+
+
+
+}
---
+++ Tablero.java
@@ -0,0 +1,48 @@
+public class Tablero
+{
+
+    public void imprimeTablero()
+      {     
+    
+
+
+
+
+       } 
+
+
+
+
+    public static void main(String args[])
+        {
+ 
+         System.out.println((char)201 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)203+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)187);
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186); 
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186);                                                                    
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186); 
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186); 
+	 System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186); 
+         System.out.println((char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219);
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 2 "  + (char)219+ " 8 "  + (char)186+ " 6 "  + (char)186+ " 1 "  + (char)186);
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186); 
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186);
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186); 
+	 System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186);  
+         System.out.println((char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219+ "" +(char)219+ "" +(char)219+ "" +(char)219 + ""+(char)219);
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186); 
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186); 
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186);  
+         System.out.println((char)186 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)206+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)186); 
+         System.out.println((char)186+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)219+ " 1 "  + (char)186+ " 1 "  + (char)186+ " 1 "  + (char)186);
+         System.out.println((char)200 + "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205+ "" +(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)219+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)202+ "" +(char)205+ "" +(char)205+ "" +(char)205 + ""+(char)188);
+        
+        }
+
+
+
+
+
+
+
+
+}
