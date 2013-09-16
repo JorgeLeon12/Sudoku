bbbaf6eb1af0e909143c93679d1bd8b639e1f077
JorgeLeon12 <jorgeleon12@hotmail.com>
2013-09-16 13:02:28 -0500

    Inconsistencia: FUNCIONA!
    Ya valora en Vertical, Horizontal y dentro de su Cuadro si hay un Numero
    Repetido, solo hay que acomodarlo en formato para que sea una funcion

--- Sudoku.java
+++ Sudoku.java
@@ -4,10 +4,11 @@ import java.util.*;
 public class Sudoku{
 	public static void main(String[] args){
 	//Imprimir Tablero
-	int sector, celda = 10000, coorX2, coorY2, sector2;
-	int coorX = Integer.parseInt(JOptionPane.showInputDialog("Introduce coordenadas en X: "));
-	int coorY = Integer.parseInt(JOptionPane.showInputDialog("Introduce coordenadas en Y: "));
-	int numeroIntro = Integer.parseInt(JOptionPane.showInputDialog("Introduce el Numero a introducir: "));
+	int sector, sector2, celda = 10000000, celda2, coorX, coorX2, coorY, coorY2, numeroIntro;
+	boolean error = false;
+	coorX = Integer.parseInt(JOptionPane.showInputDialog("Introduce coordenadas en X: "));
+	coorY = Integer.parseInt(JOptionPane.showInputDialog("Introduce coordenadas en Y: "));
+	numeroIntro = Integer.parseInt(JOptionPane.showInputDialog("Introduce el Numero a introducir: "));
 	System.out.println(coorX + " - " + coorY);//solo de Pruebas, Borrar!!
 	
 	if((coorX > 0 && coorX < 4) && (coorY > 0 && coorY < 4)){
@@ -75,52 +76,92 @@ public class Sudoku{
 	}
 	
 	
-	System.out.println("Cuadro: " + sector);//solo de Pruebas, Borrar!!
-	System.out.println("Posicion: " + celda);//solo de Pruebas, Borrar!!
 	int[][] Cas = {{5,3,0,6,0,0,0,9,8},{0,7,0,1,9,5,0,0,0},{0,0,0,0,0,0,0,6,0},{8,0,0,4,0,0,7,0,0},{0,6,0,8,0,3,0,2,0},{0,0,3,0,0,1,0,0,6},{0,6,0,0,0,0,0,0,0},{0,0,0,4,1,9,0,8,0},{2,8,0,0,0,5,0,7,9}};//solo de Pruebas, Borrar!!
 	
-	System.out.println("Numero en Casilla: " + Cas[sector][celda]);//solo de Pruebas, Borrar!!
 	
 	//inconsistencia(int sector, int celda/*, int numero*/){
-		int Buscar = 0;
-		if(sector > 3){
+		//Dentro de su cuadro
+		for(int i = 0; i < 9; i++){
+			if(Cas[sector][i] == /*this.*/numeroIntro){
+				System.out.println("Inconsistencia: EL NUMERO " + /*this.*/numeroIntro +" YA ESTA EN ESTA SECCION, INTENTA CON OTRO NUMERO.");
+				//return false;
+			}
+		}
+		//Vertical
+		if(sector > 2){
 			sector2 = sector - 3;
-			if(sector > 3){
+			if(sector2 > 2){
 				sector2 = sector2 - 3;
 			}
 		}else{
 			sector2 = sector;
 		}
-		for(int i = sector2; i < 9; i = i + 3){
-			for(int j = cfelda; j < 9; j = j + 3){
-				if(Cas[i][j] == /*this.*/numeroIntro){
-					//return false;
-					System.out.println("Inconsistencia: NUMERO REPETIDO!");
-				}
+		if(celda > 2){
+			celda2 = celda - 3;
+			if(celda2 > 2){
+				celda2 = celda2 - 3;
 			}
-			for(int j = celda; j >= 0 ; j = j - 3){
+		}else{
+			celda2 = celda;
+		}
+		for(int i = sector2; i < 9; i = i + 3){//analiza verticalmente
+			for(int j = celda2; j < 9; j = j + 3){
 				if(Cas[i][j] == /*this.*/numeroIntro){
-					//return false;
-					System.out.println("Inconsistencia: NUMERO REPETIDO!");
+					error = true;
+					System.out.println("Inconsistencia: NUMERO REPETIDO VERTICALMENTE!");
 				}
 			}
 		}
-		//return true;
+		//Horizontal
+		if(sector > 0 && sector < 3){
+			sector2 = sector - 1;
+			if(sector2 == 1){
+				sector2 = sector2 - 1;
+			}
+		}else if(sector > 3 && sector < 6){
+			sector2 = sector - 1;
+			if(sector2 == 4){
+				sector2 = sector2 - 1;
+			}
+		}else if(sector > 6 && sector < 9){
+			sector2 = sector - 1;
+			if(sector2 == 7){
+				sector2 = sector2 - 1;
+			}
+		}else{
+			sector2 = sector;
+		}
+
+		if(celda > 0 && celda < 3){
+			celda2 = celda - 1;
+			if(celda2 == 1){
+				celda2 = celda2 - 1;
+			}
+		}else if(celda > 3 && celda < 6){
+			celda2 = celda - 1;
+			if(celda2 == 4){
+				celda2 = celda2 - 1;
+			}
+		}else if(celda > 6 && celda < 9){
+			celda2 = celda - 1;
+			if(celda2 == 7){
+				celda2 = celda2	 - 1;
+			}
+		}
+
+		for(int i = sector2; i < (sector2 + 3); i++){//analiza Horizontalmente
+			for(int j = celda2; j < (celda2 + 3); j++){
+				if(Cas[i][j] == /*this.*/numeroIntro){
+					error = true;
+					System.out.println("Inconsistencia: NUMERO REPETIDO HORIZONTALMENTE!");
+				}
+			}
+		}
+		//if(error == true){
+			//return false;
+		//}else{
+			//return true;
+		//}
 	//}
-	
-//	if inco = true
-//		serValor{
-//			valorar en 9 casillas 
-//			if = true
-//				cambias
-//				
-//			else
-//				no
-//		}
-	
-	
-//	System.out.print(coorX);
-//	setValor(coorX, coorY, numeroIntro);
-		
 	}
 }
